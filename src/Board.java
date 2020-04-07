a5eee2facfae9a3348e8116801c95ab2770377d1
jewbank <jewbank@conroeisd.net>
2020-04-07 11:45:51 -0500

    Starter Code

---
+++ src/Board.java
@@ -0,0 +1,68 @@
+import java.util.ArrayList;
+
+public class Board {
+
+    /**
+     * construct a board from an N-by-N array of tiles
+     * @param tiles
+     */
+    public Board(int[][] tiles)        
+    {
+
+    }
+
+    /**
+     *
+     * @return number of blocks out of place
+     */
+    public int hamming()
+    {
+        return 0;
+    }
+
+    /**
+     *
+     * @return sum of Manhattan distances between blocks and goal
+     */
+    public int manhattan()
+    {
+        return 0;
+    }
+
+    /**
+     *
+     * @param other
+     * @return does this board equal other
+     */
+    public boolean equals(Board other)
+    {
+        return false;
+    }
+
+    /**
+     *
+     * @return an ArrayList of all neighboring board positions
+     */
+    public ArrayList<Board> neighbors()
+    {
+        return null;
+    }
+
+    /**
+     *
+     * @returna string representation of the board
+     */
+    public String toString()
+    {
+        return "";
+    }
+
+    /**
+     * test client
+     * @param args
+     */
+    public static void main(String[] args)
+    {
+
+    }
+}
---
+++ src/Solver.java
@@ -0,0 +1,42 @@
+public class Solver {
+    /**
+     * find a solution to the initial board
+     * @param initial Board to solve
+     */
+    public Solver(Board initial)
+    {
+
+    }
+
+    /**
+     * is the initial board solvable?
+     * @return
+     */
+    public boolean isSolvable()
+    {
+        return false;
+    }
+
+    /**
+     * @return min number of moves to solve the initial board; -1 if no such solution.
+     *
+     */
+    public int moves()
+    {
+        return 0;
+    }
+
+    /**
+     * a sequence of board positions that solves the puzzle in the fewest number of moves. Also print out the total number of moves and the total number of states ever enqueued.
+     * @return string representation of solution (as described above)
+     */
+    public String toString()
+    {
+        return "";
+    }
+    //  read puzzle instance from stdin and print solution to stdout (in format above)
+    public static void main(String[] args)
+    {
+
+    }
+}
